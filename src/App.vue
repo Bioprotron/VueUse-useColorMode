82d9a11039997fab49ddf555148a7b471c6d3071
unknown <brkbenjamin@yandex.ru>
2024-01-17 18:07:50 +0300

    vueuse

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { useDark, useToggle, useColorMode } from '@vueuse/core';
+const isDark = useDark(); // true or false
+const colorMode = useColorMode({
+  modes: {
+    dim: 'dim',
+    cafe: 'cafe',
+  },
+  attribute: 'theme',
+}); // 'light' or 'dark'
+const toggleDark = useToggle(isDark);
+</script>
+
+<template>
+  {{ colorMode }}
+  <button @click="colorMode = 'dark'">Dark Mode</button>
+  <button @click="colorMode = 'light'">Light Mode</button>
+  <button @click="colorMode = 'dim'">Dim Mode</button>
+  <button @click="colorMode = 'cafe'">Cafe Mode</button>
+  <button @click="toggleDark()">Toggle Dark</button>
+</template>
+<style>
+[theme='dark'] {
+  background: #252525;
+  color: white;
+}
+[theme='dim'] {
+  background: gray;
+  color: white;
+}
+[theme='cafe'] {
+  background: #c0acac;
+  color: black;
+}
+</style>
